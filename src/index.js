890c962c98617940958afb5bbab5e39c230002f1
Silas Augusto <augusto.fernandes775@gmail.com>
2020-01-20 08:21:25 -0400

    adicionando socket
    Signed-off-by: Silas Augusto <augusto.fernandes775@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
-const routes = require('./routes')
+const http = require('http');
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://silasaugusto:12237514@cluster0-hivne.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 // Métodos http: GET, POST, PUT, DELETE 
